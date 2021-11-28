941d9b0ea00044129acf100075ba98eee5d0e989
SrijitaSarkar99 <srijitasarkar9912@gmail.com>
2021-11-28 22:12:52 +0530

    created websocket server

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const { v4:uuidv4 } = require('uuid');
+
+const httpServer = http.createServer();
+// Websocket will overwrite any function we give to http here
+httpServer.listen(9090, () => console.log("Listening on 9090.."));
+//An http server is established creating a TCP connection.
+//We pass this to our websocket logic
+
+
+//hashmap for clients
+const clients = {};
+
+//Creating a websocket server
+const wsServer = new WebSocketServer({
+    "httpServer": httpServer
+});
+
+wsServer.on("request", request => {
+    //connect
+    request.accept(null, request.origin);   //accepting the connection
+    connection.on("open", () => console.log("Connection is open.."));
+    connection.on("close", () => console.log("Connection is closed"));
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data);
+        //Recieved a message from the client
+        console.log(result);
+    });
+
+    //generate a new clientID
+    const clientID = uuidv4();
+    clients[clientID] = {
+        "connection": connection
+    };
+
+    // to send back client connect
+    const payLoad = {
+        "method": "connect",
+        "clientId": clientID 
+    };
+
+    connection.send(JSON.stringify(payLoad));
+    
+});
